c0d4943af965ff267cd2f58275b36427944f1705
Martin Nagy <nagy.martin@gmail.com>
2014-07-12 15:43:34 +0200

    Remove csv, use only sqlite

--- scraper.py
+++ scraper.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python2.7
 
 import scraperwiki
-#import csv
 import urllib2
 import xlrd
 import zipfile
@@ -14,14 +13,11 @@ archive = zipfile.ZipFile(archive_file)
 
 wb = xlrd.open_workbook(file_contents=archive.read('OBCE.XLS'))
 sheet = wb.sheets()[0]
-with open('psc.csv', 'wb') as f:
-    #writer = csv.writer(f)
-    for row in range(1, sheet.nrows):
-        #writer.writerow([sheet.cell(row, i).value.encode('utf8') for i in cell_idxs])
-        data = {
-            'obec': sheet.cell(row, 1).value,
-            'okres': sheet.cell(row, 2).value,
-            'psc': sheet.cell(row, 3).value,
-            'kraj': sheet.cell(row, 7).value,
-        }
-        scraperwiki.sqlite.save(unique_keys=['obec'], data=data)
+for row in range(1, sheet.nrows):
+    data = {
+        'obec': sheet.cell(row, 1).value,
+        'okres': sheet.cell(row, 2).value,
+        'psc': sheet.cell(row, 3).value,
+        'kraj': sheet.cell(row, 7).value,
+    }
+    scraperwiki.sqlite.save(unique_keys=['obec'], data=data)
