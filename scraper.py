4a0e251ef8c1221b2843d8d9ec3bf0c74b74177d
j91321 <jan.trencansky@gmail.com>
2016-10-08 00:50:49 +0200

    dynamic filenames using re

--- scraper.py
+++ scraper.py
@@ -4,15 +4,21 @@ import scraperwiki
 import urllib2
 import xlrd
 import zipfile
+import re
 from cStringIO import StringIO
 
 url = "http://www.posta.sk/subory/322/psc-obci-a-ulic.zip"
 
 archive_file = StringIO(urllib2.urlopen(url).read())
 archive = zipfile.ZipFile(archive_file)
+archive_namelist = archive.namelist()
+r1 = re.compile('^obce.', re.IGNORECASE)
+r2 = re.compile('^ulice.', re.IGNORECASE)
+obce_filename = filter(r1.match, archive_namelist)[0]
+ulice_filename = filter(r2.match, archive_namelist)[0]
 
 # Post codes for cities
-wb = xlrd.open_workbook(file_contents=archive.read('OBCE.XLS'))
+wb = xlrd.open_workbook(file_contents=archive.read(obce_filename))
 sheet = wb.sheets()[0]
 for row in range(1, sheet.nrows):
     data = {
@@ -24,7 +30,7 @@ for row in range(1, sheet.nrows):
     scraperwiki.sqlite.save(unique_keys=['obec'], data=data, table_name="towns")
 
 # Streets
-wb = xlrd.open_workbook(file_contents=archive.read('ULICE.XLS'))
+wb = xlrd.open_workbook(file_contents=archive.read(ulice_filename))
 sheet = wb.sheets()[0]
 for row in range(1, sheet.nrows):
     data = {
